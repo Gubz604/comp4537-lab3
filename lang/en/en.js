73960cc40581309d560a7361b9260b943ba77d62
DESKTOP-9DM95V4\Gurvir <GurvirSD@Gmail.com>
2025-09-28 14:43:21 -0700

    Initial commit

---
+++ lang/en/en.js
@@ -0,0 +1,14 @@
+module.exports = {
+    welcome: "Welcome to Gurvir's Website",
+    greetingTemplate: '<p style="color: blue;">Hello %s, What a beautiful day. Server current date and time is * %s</p>',
+    writingError: 'Error writing to file',
+    writingSuccess: 'Congrats! file.txt has been appended!',
+    error404: '%s 404 File Not Found!',
+    file: 'file.txt',
+
+
+    // pathnames
+    getDate: '/getDate/',
+    writeFile: '/writeFile/',
+    readFile: '/readFile/',
+};
---
+++ server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const url = require('url');
+const util = require('util');
+const fs = require('fs');
+const date = require('./modules/utils');
+const MSG = require('./lang/en/en');
+
+PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    let q = url.parse(req.url, true);
+
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(MSG.welcome);
+    }
+
+    if (q.pathname === MSG.getDate) {                                           // getDate
+        const currentDate = date.getDate();
+        const name = q.query;
+        const message = util.format(MSG.greetingTemplate, name.name, currentDate);
+        res.end(message);
+    } 
+    else if (q.pathname === MSG.writeFile) {                                    // writeFile
+        const text = q.query;
+        fs.appendFile(MSG.file, text.text + '\n', (err) => {
+            if (err) {
+                console.log('Write Error occured!');
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                return res.end(MSG.writingError);
+            }
+            console.log('Write successful');
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            return res.end(MSG.writingSuccess);
+        });
+    } 
+    else if (q.pathname.startsWith(MSG.readFile)) {                             // readFile
+        const filename = q.pathname.replace(MSG.readFile, '');   
+        fs.readFile(filename, (err, data) => {
+            if (err) {
+                console.log('Error reading file');
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                const fileNotFound = util.format(MSG.error404, filename);
+                return res.end(fileNotFound);
+            }
+            console.log('Read Successful');
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            return res.end(data);
+        });
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
